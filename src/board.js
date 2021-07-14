354e59ba3425bd0bcaa47b710aa73e2d990d59ad
BryceTheGrand <whatkindofbunny@gmail.com>
2021-07-14 19:34:33 -0400

    init

---
+++ src/board.js
@@ -0,0 +1,217 @@
+class Board {
+  // bomb: 1
+  // empty: 0
+  // clear: 2
+  // flag: 3
+  // flag and bomb: 4
+  constructor(size, nBombs) {
+    this.size = size;
+    this.board = [];
+    this.nBombs = nBombs;
+    this.threatMap = [];
+    for (let i = 0; i < size; i++) {
+      this.board.push([]);
+      this.threatMap.push([]);
+      for (let j = 0; j < size; j++) {
+        this.board[i].push(0);
+        this.threatMap[i].push(0);
+      }
+    }
+    this.started = false;
+    this.lost = false;
+  }
+
+  restart() {
+    this.clearBoard();
+    this.started = false;
+    this.lost = false;
+  }
+
+  calcThreatMap() {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        if (this.board[i][j] == 1) {
+          this.threatMap[i][j] = -1;
+          continue;
+        }
+        let sum = 0;
+        for (let x = -1; x < 2; x++) {
+          for (let y = -1; y < 2; y++) {
+            try {
+              let newNumber = this.board[i + y][j + x];
+              if (newNumber == 1) sum += 1;
+            } catch (e) {
+              continue;
+            }
+          }
+        }
+        this.threatMap[i][j] = sum;
+      }
+    }
+  }
+
+  clearNeighbors(row, col) {
+    let checkNext = [];
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        if (
+          (i == 0 && j == 0) ||
+          i + row < 0 ||
+          i + row >= this.size ||
+          j + col < 0 ||
+          j + col >= this.size
+        )
+          continue;
+        if (
+          this.threatMap[row + i][col + j] == 0 &&
+          this.board[row + i][col + j] == 0
+        ) {
+          checkNext.push({ row: row + i, col: col + j });
+        }
+        if (this.board[row + i][col + j] == 0) {
+          this.board[row + i][col + j] = 2;
+        }
+      }
+    }
+    for (let c of checkNext) {
+      this.clearNeighbors(c.row, c.col);
+    }
+  }
+
+  generateBoard() {
+    this.clearBoard();
+    this.addBombs(this.nBombs);
+    this.calcThreatMap();
+  }
+
+  makeMove(row, col) {
+    if (!this.started) {
+      do {
+        this.generateBoard();
+        if (this.threatMap[row][col] == 0) {
+          this.started = true;
+        }
+      } while (!this.started);
+    }
+    if (!this.lost) {
+      if (this.board[row][col] == 0) {
+        this.board[row][col] = 2;
+        if (this.threatMap[row][col] == 0) {
+          this.clearNeighbors(row, col);
+        }
+      } else if (this.board[row][col] == 1) {
+        this.lost = true;
+      }
+    }
+  }
+
+  flag(row, col) {
+    switch (this.board[row][col]) {
+      case 0:
+        this.board[row][col] = 3;
+        break;
+      case 1:
+        this.board[row][col] = 4;
+        break;
+      case 3:
+        this.board[row][col] = 0;
+        break;
+      case 4:
+        this.board[row][col] = 1;
+        break;
+    }
+  }
+
+  show(ctx, x, y, w, h) {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        switch (this.board[i][j]) {
+          case 0:
+            ctx.fillStyle = "rgb(25, 20, 20)";
+            break;
+          case 1:
+            ctx.fillStyle = "rgb(25, 20, 20)";
+            if (this.lost) {
+              ctx.fillStyle = "rgb(180, 30, 50)";
+            }
+            break;
+          case 2:
+            ctx.fillStyle = "rgba(205, 200, 195, 0)";
+
+            ctx.font = 0.9 * (w / this.size) + "px Arial";
+            let temp = ctx.fillStyle;
+            let tempW = ctx.lineWidth;
+            ctx.fillStyle = "rgb(245, 230, 220)";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText(
+              this.threatMap[i][j] != 0 ? this.threatMap[i][j] : "",
+              ((j + 0.5) * w) / this.size,
+              ((i + 0.56) * h) / this.size
+            );
+            ctx.fillStyle = temp;
+            ctx.lineWidth = tempW;
+            break;
+          case 3:
+            ctx.fillStyle = "rgb(80, 120, 100)";
+            break;
+          case 4:
+            ctx.fillStyle = "rgb(80, 120, 100)";
+            break;
+          default:
+            ctx.fillStyle = "rgb(0, 255, 255)";
+            break;
+        }
+
+        ctx.beginPath();
+        ctx.rect(
+          x + (j * w) / this.size,
+          y + (i * h) / this.size,
+          w / this.size,
+          h / this.size
+        );
+        ctx.fill();
+      }
+    }
+
+    for (let i = 1; i < this.size; i++) {
+      ctx.beginPath();
+      ctx.moveTo(x + (i * w) / this.size, y);
+      ctx.lineTo(x + (i * w) / this.size, y + h);
+      ctx.stroke();
+    }
+    for (let j = 1; j < this.size; j++) {
+      ctx.beginPath();
+      ctx.moveTo(x, y + (j * h) / this.size);
+      ctx.lineTo(x + w, y + (j * h) / this.size);
+      ctx.stroke();
+    }
+  }
+
+  clearBoard() {
+    this.board = [];
+    for (let i = 0; i < this.size; i++) {
+      this.board.push([]);
+      for (let j = 0; j < this.size; j++) {
+        this.board[i].push(0);
+      }
+    }
+  }
+
+  addBombs(nBombs) {
+    for (let i = 0; i < nBombs; i++) {
+      let found = false;
+      do {
+        let pos = {
+          row: Math.floor(Math.random() * this.size),
+          col: Math.floor(Math.random() * this.size),
+        };
+        if (this.board[pos.row][pos.col] == 0) {
+          found = true;
+          this.board[pos.row][pos.col] = 1;
+        }
+      } while (!found);
+    }
+    this.calcThreatMap();
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,82 @@
+let canvas = document.getElementById("game");
+let ctx = canvas.getContext("2d");
+let gameSize = 20;
+let nBombs = Math.floor(0.15 * 30 * 30);
+let gameBoard = new Board(gameSize, nBombs);
+let canvasDOM = canvas.getBoundingClientRect();
+let restartButton = document.getElementById("restart");
+restartButton.addEventListener("click", () => {
+  gameBoard.restart();
+});
+
+let numberBombsSlider = document.getElementById("number-bombs");
+numberBombsSlider.addEventListener("change", () => {
+  nBombs = Math.floor((numberBombsSlider.value / 100) * 30 * 30);
+  gameBoard = new Board(gameSize, nBombs);
+});
+
+let boardSizeSlider = document.getElementById("board-size");
+boardSizeSlider.addEventListener("change", () => {
+  gameSize = boardSizeSlider.value;
+  gameBoard = new Board(gameSize, nBombs);
+});
+
+canvas.addEventListener("click", (evt) => {
+  let pos = getPos(evt);
+  gameBoard.makeMove(pos.y, pos.x);
+});
+
+canvas.addEventListener("contextmenu", (evt) => {
+  evt.preventDefault();
+  let pos = getPos(evt);
+  gameBoard.flag(pos.y, pos.x);
+});
+
+window.addEventListener("resize", () => {
+  canvasDOM = canvas.getBoundingClientRect();
+});
+
+function getPos(evt) {
+  return {
+    x: Math.floor((gameSize * (evt.clientX - canvasDOM.x)) / canvasDOM.width),
+    y: Math.floor((gameSize * (evt.clientY - canvasDOM.y)) / canvasDOM.height),
+  };
+}
+
+function setup() {
+  window.requestAnimationFrame(loop);
+}
+
+function background(r, g, b) {
+  ctx.beginPath();
+  ctx.rect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";
+  ctx.fill();
+}
+
+function roundedRect(x, y, w, h, r) {
+  ctx.beginPath();
+  ctx.moveTo(x + r, y);
+  ctx.lineTo(x + w - r, y);
+  ctx.arc(x + w - r, y + r, r, 1.5 * Math.PI, 2 * Math.PI);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.arc(x + w - r, y + h - r, r, 0, 0.5 * Math.PI);
+  ctx.lineTo(x + r, y + h);
+  ctx.arc(x + r, y + h - r, r, 0.5 * Math.PI, Math.PI);
+  ctx.lineTo(x, y + r);
+  ctx.arc(x + r, y + r, r, Math.PI, 1.5 * Math.PI);
+  ctx.stroke();
+}
+
+function loop() {
+  background(60, 55, 55);
+
+  ctx.strokeStyle = "rgba(250, 245, 240, 0.6)";
+  ctx.lineWidth = 2;
+  gameBoard.show(ctx, 0, 0, canvas.width, canvas.height);
+  roundedRect(1, 1, canvas.width - 2, canvas.height - 2, canvas.width * 0.02);
+
+  window.requestAnimationFrame(loop);
+}
+
+setup();
